aa3756008c0fcdb6ad31c19df08b086566042806
namevug <ciavug@gmail.com>
2023-02-15 00:56:01 -0300

    feat: Object

---
+++ fundamentos/12_objeto.js
@@ -0,0 +1,21 @@
+/*      c0d3r
+
+    * Object
+
+    - 
+
+*/
+
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 49.99
+prod1['Desconto Legal'] = 0.40      // Evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camiseta Polo',
+    preco: 79.90
+}
+
+console.log(prod2)
