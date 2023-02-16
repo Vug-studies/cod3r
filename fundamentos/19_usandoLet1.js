e096f6ce0135fdd5f15a9407e76f62bf6f707206
namevug <ciavug@gmail.com>
2023-02-15 22:32:03 -0300

    feat: let

---
+++ fundamentos/19_usandoLet1.js
@@ -0,0 +1,10 @@
+let numero = 1
+{
+    let numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero)
+
+// Variável definida com a palavra reservada "var", tem escopo global e de função.
+
+// Variável definida com a palavra reservada "let", tem escopo global, escopo de função e escopo de bloco.
