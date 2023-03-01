426f7800e84e0e06f791d8bc8e15160090f11eca
namevug <ciavug@gmail.com>
2023-02-28 23:54:02 -0300

    feat: Tratamento de Erro

---
+++ fundamentos/34_erro.js
@@ -0,0 +1,11 @@
+function tratarErroLancar(erro) {
+    // throw new Error('...')
+    // throw 10
+    // throw true
+    // throw 'mensagem'
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
