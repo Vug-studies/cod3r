7932661e7439a598daf249651e3fdb1c52359e3b
namevug <ciavug@gmail.com>
2023-03-10 21:59:36 -0300

    feat: firstFunction

---
+++ 03_Funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,12 @@
+// Função em JS é First-Class Object (Citizens)
+// Higher-order function
+
+// criar de forma literal
+function fun1() { }
+
+// Armazenar em uma variável
+const fun2 = function () { }
+
+// Armazenar em um array
+const array = [function (a, b) { return a + b }, fun1, fun2]
+console.log(array[0](2, 3))
